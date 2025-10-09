0349bbff78ed51d63e4f0239f0084230e7feac57
tshihab07 <tushar.shihab13@gmail.com>
2025-10-09 23:59:31 +0600

    feat: update form definitions for user input handling

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,3 @@
-# app/forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, IntegerField, PasswordField, validators
 from wtforms.validators import DataRequired, Email, NumberRange, Length
